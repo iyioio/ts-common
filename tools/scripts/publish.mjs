2261d375f5a02b88494d2f2203b05ee91edacadb
Scott Vance <scott@iyio.io>
2022-10-01 16:29:30 -0400

    - Updated publish script to use version in package.json

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -21,14 +21,8 @@ function invariant(condition, message) {
 
 // Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
 // Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name, version, tag = 'next'] = process.argv;
+const [, , name] = process.argv;
 
-// A simple SemVer validation to validate the version
-const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
-invariant(
-    version && validVersion.test(version),
-    `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
-);
 
 const graph = readCachedProjectGraph();
 const project = graph.nodes[name];
@@ -46,18 +40,5 @@ invariant(
 
 process.chdir(outputPath);
 
-// Updating the version in "package.json" before publishing
-try {
-    const json = JSON.parse(readFileSync(`package.json`).toString());
-    json.version = version;
-    writeFileSync(`package.json`, JSON.stringify(json, null, 2));
-} catch (e) {
-    console.error(
-        chalk.bold.red(
-            `Error reading package.json file from library build output.`
-        )
-    );
-}
-
 // Execute "npm publish" to publish
-execSync(`npm publish --access public --tag ${tag}`);
+execSync(`npm publish --access public`);
